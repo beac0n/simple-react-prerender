03ad361e306521fa15d2bc8db863e7eff3783499
beac0n <mcschempp@gmx.de>
2017-05-01 15:09:09 +0200

    refactor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const printUtil = require('./printUtil')
-const steps = require('./steps')
+const printUtil = require('./src/printUtil')
+const steps = require('./src/steps')
 
 process.env.NODE_ENV = 'production'
 process.env.ON_SERVER = 'true'
